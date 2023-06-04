205d490d15af802ece249161681a107e99d93c6f
DuduWater12 <duarte_afonso48@hotmail.com>
2023-06-04 16:04:13 +0100

    Added SQL queries to display all Exams and a procedure to find them by a given school

---
+++ Queries SQL/Procedures/ExamesEmEscola.sql
@@ -0,0 +1,26 @@
+CREATE PROCEDURE ExamesDeAlunosDaEscola @Escola VARCHAR(255)
+AS
+SELECT *
+    FROM ExamesRealizados
+    WITH ExamesRealizados AS(
+    SELECT RE.id AS "Id",
+        RE.fk_aluno AS "Nº de Aluno", 
+        A.Nome AS "Nome", 
+        AE.Nome AS "Escola do Aluno", 
+        EE.Nome AS "Escola do Exame", 
+        CONCAT(D.Nome, ' -  Fase ', EN.Fase, ' ', EN.Ano) AS "Exame",
+        EN RE.NotaFinal AS "Nota Final", 
+        RE.NotaRevisada AS "Nota Revisada"
+    FROM 
+        RealizacaoExame AS RE JOIN Aluno AS A 
+            ON RE.fk_Aluno=A.NrAluno AND RE.fk_AlunoEscola=A.idEscola
+        JOIN ExameNacional EN 
+            ON RE.fk_ExameNacional=EN.id
+        JOIN AlunoEscola AE 
+            ON RE.fk_AlunoEscola=E.id
+        JOIN EscolaExame EE 
+            ON RE.fk_EscolaRealizada=EE.id
+        JOIN Disciplina D 
+            ON EN.fk_Disciplina=D.id
+    )
+    WHERE ExamesRealizados.AE.Nome = @Escola;
