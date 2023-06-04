29f02483a5a28d9324d1d8f5a6105c5e1da1d47b
chico ferreira <36338391+chicoferreira@users.noreply.github.com>
2023-06-04 14:59:16 +0100

    Script pede informações da conexão

--- importdataset.py
+++ importdataset.py
@@ -1,13 +1,32 @@
 import csv
 import mysql.connector
 
+# Função para carregar as configurações da conexão com a base de dados a partir de um input
+def load_config_from_input(input_str):
+    config = {}
+    parts = input_str.split('@')
+
+    def invalid_format():
+        raise ValueError("Formato inválido. O input deve estar no formato user:password@host:database")
+
+    if len(parts) != 2:
+        invalid_format()
+    
+    user_password = parts[0].split(':')
+    host_database = parts[1].split(':')
+
+    if len(user_password) != 2 or len(host_database) != 2:
+        invalid_format()
+    
+    config['user'] = user_password[0]
+    config['password'] = user_password[1]
+    config['host'] = host_database[0]
+    config['database'] = host_database[1]
+    
+    return config
+
 # Configurações da conexão com a base de dados
-config = {
-    'user': 'root',
-    'password': 'admin',
-    'host': 'localhost',
-    'database': 'edudata'
-}
+config = load_config_from_input(input('Conexão com a base de dados (user:password@host:database): '))
 
 # Função para estabelecer a conexão com a base de dados
 def conectar():
